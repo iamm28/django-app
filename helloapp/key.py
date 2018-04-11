a0ce6a779188eb6092063ad107cd297bec68aa36
iamm28 <alisonmackay246@gmail.com>
2018-04-11 13:04:43 -0400

    first commit

---
+++ helloapp/key.py
@@ -0,0 +1,1 @@
+key = '@it^=)p)s2m@1+^2oh3&&fv%tj(6^hw!6$z66h_e!b-9-hm-u)'
---
+++ howdy/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, re_path
+from howdy import views
+
+urlpatterns = [
+    re_path(r'^$', views.HomePageView.as_view()),
+]
